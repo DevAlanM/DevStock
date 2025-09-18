f6d73bbc07d6f96abdb8dc5a8ac94637d8eb1247
Dev Aptare - Alan Martins <alanmartins@aptaretecnologia.com.br>
2025-09-18 13:11:04 -0300

    project created

---
+++ src/main/java/com/devalan/devstock/DevstockApplication.java
@@ -0,0 +1,13 @@
+package com.devalan.devstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devalan/devstock/DevstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devalan.devstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
